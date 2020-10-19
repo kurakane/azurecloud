1fb9fc9aaf1419eda0cb8cb78d874c7747598ff3
kurakane <kurakane.takashi@outlook.com>
2020-10-19 16:39:24 +0900

    不要な定義を削除

--- src/cfg.py
+++ src/cfg.py
@@ -1,17 +1,10 @@
 # 各種定義ファイル.
 
-# AzureStrageのアカウント名.
-STORAGE_ACCOUNT_NAME = 'kurakanestrage'
-# AzureStrageのアクセスキー.
-STORAGE_ACCOUNT_KEY = 'vuMEUJwlfdHxnFsaCTMgW/zedeZA+k5do519n8OXE9QUW+J6aYQvHuoYvi5q/7aH7ylEEIixinWUEQObQH+WlQ=='
-
 # 入力ファイルのAzureStorageコンテナ名.(パス形式)
 STORAGE_CONTAINER_INPUT = '/data-in/'
 # 出力ファイルのAzureStorageコンテナ名.(パス形式)
 STORAGE_CONTAINER_OUTPUT = '/data-out/'
 
-# アップロード時のローカルファイルのファイル名.
-FILE_TMP = 'tmp.dmp'
 # 休日情報のファイル名. (クライアント側と合わせること)
 FILE_HOLIDAYS = 'Holidays'
 # 検索条件のファイル名. (クライアント側と合わせること)
