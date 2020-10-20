43a556af0d1d643d7ea88cb8609e2d736bef907f
kurakane <kurakane.takashi@outlook.com>
2020-10-20 18:25:15 +0900

    デバッグ用のログを追加

--- src/util.py
+++ src/util.py
@@ -1,10 +1,12 @@
 """タスク間で共有して使用する共通処理."""
 
 import bz2
+import datetime
 import os
 import pickle
 import shutil
 import sys
+import time
 
 import cfg
 
@@ -49,6 +51,17 @@ def print_env():
     """デバッグ用に環境変数を出力する."""
     print(f'ノード:[{get_env("AZ_BATCH_NODE_ID")}]')
     print(f'タスクID:[{get_env("AZ_BATCH_TASK_ID")}]')
+    print('--')
+
+
+def print_sec(start_time):
+    """デバッグ用に秒数を出力する."""
+    print(f'[{(time.time() - start_time):,.3f}] sec')
+
+
+def print_file_size(file_path):
+    """デバッグ用にファイルサイズを出力する."""
+    print(f'[{(os.path.getsize(file_path) / 1024 / 1024):,.3f}]  MB')
 
 
 def load_bz2_file(file_path, file_name):
@@ -63,20 +76,25 @@ def load_bz2_file(file_path, file_name):
         # 解凍するために一時ファイルを準備.
         file_path_pickle = os.path.join(get_workdir(), file_name + '.tmp')
         # 解凍した一時ファイルをローカルに書き込む.
+        start_time = time.time()
         with open(file_path_pickle, 'wb') as f2:
-            print(f'一時ファイルを展開します. [{file_path_pickle}]')
+            print(f'一時ファイルを展開します. [{file_path_pickle}]', end='')
             f2.write(bz2.decompress(f.read()))
+        print(' <OK>')
+        print_sec(start_time)
+        print_file_size(file_path_pickle)
 
     # 解凍ファイルからloadする.
-    print(f'ファイルをloadします. [{file_path_pickle}]')
+    start_time = time.time()
+    print(f'ファイルをloadします. [{file_path_pickle}]', end='')
     with open(file_path_pickle, 'rb') as f:
         data = pickle.load(f)
-
         # ローカルの一時ファイルを削除する.
-        print(f'一時ファイルを削除します. [{file_path_pickle}]')
         os.remove(file_path_pickle)
 
-        return data
+    print(' <OK>')
+    print_sec(start_time)
+    return data
 
 
 def upload_bz_file(file_path, file_name, obj):
@@ -84,28 +102,41 @@ def upload_bz_file(file_path, file_name, obj):
 
     # アップロードファイルをローカルに出力する.
     file_path_tmp = os.path.join(get_workdir(), file_name + '.tmp')
-    print(f'ファイルを出力します. [{file_path_tmp}]')
+    start_time = time.time()
+    print(f'ファイルをNodeローカルに出力します. [{file_path_tmp}]', end='')
     with open(file_path_tmp, 'wb') as f:
         pickle.dump(obj, f)
+    print(' <OK>')
+    print_sec(start_time)
+    print_file_size(file_path_tmp)
 
     # アップロードファイルをBZ2で圧縮する.
     file_path_bz2 = os.path.join(get_workdir(), file_name + '.bz2')
-    print(f'ファイルをBZ2圧縮します. [{file_path_tmp}] -> [{file_path_bz2}]')
+    start_time = time.time()
+    print(f'ファイルをBZ2圧縮します. [{file_path_tmp}] -> [{file_path_bz2}]', end='')
     with open(file_path_tmp, 'rb') as f:
         data = f.read()
         with open(file_path_bz2, 'wb') as f2:
             f2.write(bz2.compress(data))
+    print(' <OK>')
+    print_sec(start_time)
+    print_file_size(file_path_bz2)
 
     # BZ2ファイルをAzureStorageにアップロード(コピー)する.
     fil_path_output = os.path.join(file_path, file_name + '.bz2')
     if not os.path.exists(file_path):
         os.mkdir(file_path)
 
-    print(f'ファイルをコピーします. [{file_path_bz2}] -> [{fil_path_output}]')
+    start_time = time.time()
+    print(f'ファイルをAzureStorageへコピーします. [{file_path_bz2}] -> [{fil_path_output}]', end='')
     shutil.copy(file_path_bz2, fil_path_output)
+    print(' <OK>')
+    print_sec(start_time)
 
     # ローカルの一時ファイルを削除する.
-    print(f'一時ファイルを削除します. [{file_path_tmp}]')
+    print(f'一時ファイルを削除します. [{file_path_tmp}]', end='')
     os.remove(file_path_tmp)
-    print(f'一時ファイルを削除します. [{file_path_bz2}]')
+    print(' <OK>')
+    print(f'一時ファイルを削除します. [{file_path_bz2}]', end='')
     os.remove(file_path_bz2)
+    print(' <OK>')
