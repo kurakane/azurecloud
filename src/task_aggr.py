435eaffa30ce7040e87de3f919f0d9403364fe35
kurakane <kurakane.takashi@outlook.com>
2020-10-20 16:48:19 +0900

    バグfix

--- src/task_aggr.py
+++ src/task_aggr.py
@@ -16,12 +16,13 @@ def run():
     # コマンドライン引数を受け取る.
     args = sys.argv
 
-    for arg in args:
+    for i, arg in enumerate(args):
         # NPVファイルを読込する.
-        path_npv = os.path.join(util.get_outputdir(), arg, cfg.FILE_NPV)
-        print(f'NPVファイルを読込します [{path_npv}]')
-        df = pd.read_csv(path_npv)
-        print(df.info())
+        if i:
+            path_npv = os.path.join(util.get_outputdir(), arg, cfg.FILE_NPV)
+            print(f'NPVファイルを読込します [{path_npv}]')
+            df = pd.read_csv(path_npv)
+            df.info()
 
     print('正常終了しました.')
 
